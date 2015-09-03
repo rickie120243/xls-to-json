7eeda65ed2e8f0550136b022ace1ba756720ada8
rickie120243 <rickie120243@gmail.com>
2015-09-03 15:57:09 +0800

    first commit

---
+++ xls-to-json.js
@@ -0,0 +1,25 @@
+var input="biography of sutra 3";  // input folder name
+
+/////////////////////////////////////////////////////
+var fs=require("fs");
+var xls_json=require("xls-to-json");
+var mkdirp=require("mkdirp");
+var path=require("path").dirname;
+var list=fs.readdirSync(input);
+
+var xlstojson=function(xls) {
+	var name=xls.split(".");
+	var outfn="out/"+name[0];
+	var dir=path(outfn);
+	mkdirp.sync(dir);
+	xls_json({
+	  input: __dirname + "/"+input+"/"+name[0]+".xls",
+	  output: __dirname + "/out/"+name[0]+".json"
+	}, function(err, result) {
+	  
+	  if(err) {
+	    console.error(err);
+	  } 
+	});
+} 
+list.map(xlstojson);
