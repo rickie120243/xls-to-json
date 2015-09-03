3b993c69c88ce7db01ae280a04710dc8e7b8f786
rickie120243 <rickie120243@gmail.com>
2015-09-03 16:40:31 +0800

    add xlsx-to-json

---
+++ xlsx-to-json.js
@@ -0,0 +1,25 @@
+var input="biography of sutra 3";  // input folder name
+
+/////////////////////////////////////////////////////
+var fs=require("fs");
+var xlsx_json=require("xlsx-to-json");
+var mkdirp=require("mkdirp");
+var path=require("path").dirname;
+var list=fs.readdirSync(input);
+
+var xlstojson=function(xlsx) {
+	var name=xlsx.split(".");
+	var outfn="out/"+name[0];
+	var dir=path(outfn);
+	mkdirp.sync(dir);
+	xlsx_json({
+	  input: __dirname + "/"+input+"/"+name[0]+".xlsx",
+	  output: __dirname + "/out/"+name[0]+".json"
+	}, function(err, result) {
+	  
+	  if(err) {
+	    console.error(err);
+	  } 
+	});
+} 
+list.map(xlsxtojson);
